a23451a56b4800b2e73264c9e5551b0a553d4f0f
Muayyad Alsadi <alsadi@gmail.com>
2013-05-26 17:11:38 +0300

    apply many phpcs suggestions to HttpClient

--- HttpClient.php
+++ HttpClient.php
@@ -1,16 +1,44 @@
 <?php 
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * HttpClient is a simple minimal cUrl-based HTTP client
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 2.0 of the Apache license
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * @category  Services
+ * @package   LiteSolr
+ * @author    Muayyad Alsadi <alsadi@gmail.com>
+ * @copyright 2013 Muayyad Alsadi
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @version   GIT: <git_id>
+ * @link      http://pear.php.net/package/PackageName
+ */
+
 
 /**
  * Simple HTTP client based on cUrl, it can re-use connections
+ * 
  * it support all kinds of custom HTTP methods like PUT, PURGE and DELETE
  * used like this
  * $http=new HttpClient();
  * list($info, $content)=$http->get($url, array($key1=>$value1, $key2=>$value2));
  * list($info, $content)=$http->post($url, array($key1=>$value1, $key2=>$value2));
- * 
- **/
-class HttpClient {
-  protected $_opt = array(
+ *
+ * @category  Services
+ * @package   LiteSolr
+ * @author    Muayyad Alsadi <alsadi@gmail.com>
+ * @copyright 2013 Muayyad Alsadi
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PackageName
+ */
+class HttpClient
+{
+    protected $opt = array(
         CURLOPT_USERAGENT=>'LiteSolr (cUrl)',
         CURLOPT_HEADER => 0,
         CURLOPT_HTTPHEADER=>array('Expect: '),
@@ -19,80 +47,113 @@ class HttpClient {
         CURLOPT_BINARYTRANSFER => 1,
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_TIMEOUT => 600,
-  );
-  protected $_curl=null;
-  
-  public function __construct($options=array()) {
-	   $this->_curl = curl_init();
-	   $this->_opt = $options + $this->_opt;
-  }
-
-  public function __destruct()
-  {
-    if(gettype($this->_curl) == 'resource') curl_close($this->_curl);
-  }
-
-/**
- * Send a POST requst using cURL
- * @param string $url to request
- * @param array $params values to send
- * @param array $options for cURL
- * @return info array, result string
- */
-public function post($url, $params = array(), $options = array())
-{
-    $options += array(
-        CURLOPT_POST => 1,
-        CURLOPT_URL => $url,
-        CURLOPT_POSTFIELDS => (is_string($params))?$params:http_build_query($params),
-    ) + $this->_opt;
-    //curl_reset($this->_curl);
-    curl_setopt_array($this->_curl, $options);
-    if( ! $result = curl_exec($this->_curl))
-    {
-        return array(array('code'=>'', 'Content-Type'=>'', 'curl-Error'=>curl_error($ch)), '');
-    }
-    $info=array(
-      'code'=>curl_getinfo ($this->_curl, CURLINFO_HTTP_CODE),
-      'Content-Type'=>curl_getinfo ($this->_curl, CURLINFO_CONTENT_TYPE)
     );
-    //curl_close($this->_curl);
-    return array($info, $result);
-}
+    protected $curl=null;
 
-/**
- * Send a GET requst using cURL
- * @param string $url to request
- * @param array $params values to send
- * @param array $options for cURL
- * @return string
- */
-public function get($url, array $params = array(), array $options = array())
-{   
-    $options += array(
-        CURLOPT_URL => $url. (($params)?((strpos($url, '?') === FALSE ? '?' : ''). http_build_query($params)):''),
-    ) + $this->_opt;
-    curl_setopt_array($this->_curl, $options);
-    if( ! $result = curl_exec($this->_curl))
+    /**
+     * Constructor
+     * 
+     * @param array $options cUrl options common in all requests
+     *
+     * @return a new instance
+     **/
+    public function __construct($options=array())
     {
-        return array(array('code'=>'', 'Content-Type'=>'', 'curl-Error'=>curl_error($this->_curl)), '');
+        $this->curl = curl_init();
+        $this->opt = $options + $this->opt;
     }
-    $info=array(
-      'code'=>curl_getinfo ($this->_curl, CURLINFO_HTTP_CODE),
-      'Content-Type'=>curl_getinfo ($this->_curl, CURLINFO_CONTENT_TYPE)
-    );
-    return array($info, $result);
-}
+
+    /**
+     * Destructor
+     **/
+    public function __destruct()
+    {
+        if (gettype($this->curl) == 'resource') {
+            curl_close($this->curl);
+        }
+    }
+
+    /**
+     * Send a POST requst using cURL
+     *
+     * @param string $url     to request
+     * @param array  $params  values to send
+     * @param array  $options for cURL
+     *
+     * @return array having two elements info and result
+     */
+    public function post($url, $params = array(), $options = array())
+    {
+        $options += array(
+            CURLOPT_POST => 1,
+            CURLOPT_URL => $url,
+            CURLOPT_POSTFIELDS => (is_string($params))?
+                $params:http_build_query($params),
+        ) + $this->opt;
+        curl_setopt_array($this->curl, $options);
+        if (! $result = curl_exec($this->curl)) {
+            return array(array(
+                'code'=>'',
+                'Content-Type'=>'',
+                'curl-Error'=>curl_error($ch)), ''
+            );
+        }
+        $info=array(
+          'code' => curl_getinfo($this->curl, CURLINFO_HTTP_CODE),
+          'Content-Type' => curl_getinfo($this->curl, CURLINFO_CONTENT_TYPE)
+        );
+        return array($info, $result);
+    }
+    
+    /**
+     * Send a GET requst using cURL
+     *
+     * @param string $url     to request
+     * @param array  $params  values to send
+     * @param array  $options for cURL
+     *
+     * @return array having two elements info and result
+     */
+    public function get($url, array $params = array(), array $options = array())
+    {   
+        if ($params) {
+            $url.=(strpos($url, '?') === false ? '?' : '')
+                .http_build_query($params));
+        }
+        $options += array(
+            CURLOPT_URL => $url,
+        ) + $this->opt;
+        curl_setopt_array($this->curl, $options);
+        if (! $result = curl_exec($this->curl)) {
+            return array(array(
+                'code'=>'',
+                'Content-Type'=>'',
+                'curl-Error'=>curl_error($ch)), ''
+            );
+        }
+        $info=array(
+          'code' => curl_getinfo($this->curl, CURLINFO_HTTP_CODE),
+          'Content-Type' => curl_getinfo($this->curl, CURLINFO_CONTENT_TYPE)
+        );
+        return array($info, $result);
+    }
 
 
     /**
-     * call unusual method like PUT, DELETE, PURGE ..etc.
-     * */
-    public function __call($method,$args){
-		$params=shift($args);
-		$options=shift($args);
-		$options[CURLOPT_CUSTOMREQUEST]=strtoupper($method);
-		return $this->post($url, $params, $options);
+     * Send a custom unusual method using cURL (eg. PUT, DELETE, PURGE, ..etc.)
+     *
+     * @param string $method HTTP method
+     * @param array  $args   options like post above
+     *
+     * @return array having two elements info and result
+     */
+    public function __call($method,$args)
+    {
+        // FIXME: check length of $args
+        $params=shift($args);
+        $options=shift($args);
+        $options[CURLOPT_CUSTOMREQUEST]=strtoupper($method);
+        return $this->post($url, $params, $options);
     }
 
 }
